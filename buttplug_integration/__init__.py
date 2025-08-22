6c02cc31c439f1485d9147cf84486d6443e002a6
James Alex Griffin <113137474+belgriffinite@users.noreply.github.com>
2025-08-22 17:22:11 +0000

    Aug 22, 2025, 10:22 AM

---
+++ buttplug_integration/__init__.py
@@ -0,0 +1,7 @@
+"""Buttplug integration package for StrokeGPT 2.0 (skeleton)."""
+
+from .translator import ButtplugTranslator
+from .bridge import ButtplugBridge
+from .devices import DeviceSpec
+
+__all__ = ["ButtplugTranslator", "ButtplugBridge", "DeviceSpec"]
